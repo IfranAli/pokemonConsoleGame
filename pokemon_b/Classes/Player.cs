89f0a59df3cc390b43f564397783461c67cfea0a
IfranAli <tehpanda32@gmail.com>
2015-06-17 04:41:45 +1000

    update

---
+++ pokemon_b/Classes/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pokemon_b
+{
+	public class player : Trainer
+	{
+		public player (String playerName)
+			:base(playerName) {
+
+		}
+
+		public override void PerformTurn(Trainer opponent) {
+			Console.WriteLine ("Hey Hey Hey!!");
+		}
+	}
+}
+
